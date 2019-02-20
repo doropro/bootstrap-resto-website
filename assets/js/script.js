5e06194d0777dbf33dd62f239d8122254813ece8
doropro <weiss.dorothee.pro@gmail.com>
2019-02-20 22:09:54 +0100

    mise en forme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,20 @@
-/*var maDate = new Date();
-var semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
-var j = semaine[maDate.getDay()-1];
-var an = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
-var mo = an[maDate.getMonth()+1];
-var a = maDate.getFullYear()
-var h = maDate.getHours();
-var m = maDate.getMinutes();
-var affich = "Nous sommes le" + " " +j+  +mo+ "/" +a+ " " + "et il est" + " " +h+ ":" +m;*/
+//date +hours
+
+(function() {
 var d = new Date();
+var semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+var jour = semaine[d.getDay()-1];
+var date = d.getDate();
+var an = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+var mois = an[d.getMonth()];
+var annee = d.getFullYear();
+var heure = d.getHours();
+var minute = d.getMinutes();
 
-  var semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
-
-  var jour = semaine[d.getDay()-1];
-
-  var datec = d.getDate();
-
-  var an = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
-
-  var mois = an[d.getMonth()];
-
-  var annee = d.getFullYear();
-
-  var heure = d.getHours();
-
-  var minute = d.getMinutes();
-
-  /*function pad2(number) {
-return (number < 10 ? '0' : '') + number;
-}*/
-  document.getElementById("timer").innerHTML = ("Nous sommes le" + " " + jour + " " + datec + " " + mois + " " + annee + ", " + heure + "h" + minute);
+  function pad2(minute) { // 0 before minutes
+return (minute < 10 ? '0' : '') + minute;
+}
+  document.getElementById("timer").innerHTML = ("Nous sommes le" + " " + jour + " " + date + " " + mois + " " + annee + ", " + heure + "h" + minute);
 
   if (heure < 12) {
       document.getElementById("open").innerHTML = "Nous sommes actuellement fermés";
@@ -39,16 +25,4 @@ return (number < 10 ? '0' : '') + number;
       else {
     document.getElementById("open").innerHTML = "Nous sommes actuellement ouvert";
   }
-
-
-
-
- /*document.getElementById("Timer").innerHTML  = affich
-
-if (h < 12 || h > 23) {
-  console.log(1);
-}
-else {
-  console.log(2);
-}
-*/
+})();
---
+++ assets/js/script2.js
@@ -0,0 +1,6 @@
+// hello + name user
+
+    var start = 'Bonjour ', userName, end= '!', result;
+    userName = prompt ('entrez votre prénom:');
+    result = start + userName + end;
+    alert (result);
