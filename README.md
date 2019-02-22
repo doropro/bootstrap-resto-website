7597dc7059a29d4650d23691a48f4fe403fbff44
doropro <weiss.dorothee.pro@gmail.com>
2019-02-22 15:30:38 +0100

    amelioration photos

--- README.md
+++ README.md
@@ -21,7 +21,8 @@ Terminé
 ## Liens utiles :
 
 lien des consignes : <https://github.com/becodeorg/BXL-Johnson-3.9/tree/master/Projets/Restaurant_Bootstrap>
+
 lien github page : <https://doropro.github.io/bootstrap-resto-website/>
 ## Aperçu du site :
 
-![Apercu jeu](assets/img/Capture du 2019-02-21 16-20-29.png)
+![Apercu site](assets/img/Capture du 2019-02-21 16-20-29.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var minute = d.getMinutes();
   function pad2(minute) { // 0 before minutes
 return (minute < 10 ? '0' : '') + minute;
 }
-  document.getElementById("timer").innerHTML = ("Nous sommes le" + " " + jour + " " + date + " " + mois + " " + annee + ", " + heure + "h" + minute);
+  document.getElementById("timer").innerHTML = ("Nous sommes le" + " " + jour + " " + date + " " + mois + " " + annee + ", " + heure + "h" + pad2(minute));
 
   if (heure < 12) {
       document.getElementById("open").innerHTML = "Nous sommes actuellement fermés";
